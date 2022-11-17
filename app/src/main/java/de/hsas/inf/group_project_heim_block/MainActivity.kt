bea94acfecef4d3db4b4ed012390f238e7240466
blockmel <blockmel@hs-albsig.de>
2022-11-17 18:55:07 +0000

    Initial commit

---
+++ app/src/main/java/de/hsas/inf/group_project_heim_block/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.hsas.inf.group_project_heim_block
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
