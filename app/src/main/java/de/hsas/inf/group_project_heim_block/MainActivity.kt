2cb458c64264e05fb571b6c07b708cc61b337d56
blockmel <blockmel@hs-albsig.de>
2022-11-27 13:37:02 +0000

    add sign-up fields

--- app/src/main/java/de/hsas/inf/group_project_heim_block/MainActivity.kt
+++ app/src/main/java/de/hsas/inf/group_project_heim_block/MainActivity.kt
@@ -14,14 +14,16 @@ class MainActivity : AppCompatActivity() {
 
         val db = Firebase.firestore
 
-        // Create a new user with a first and last name
+
+// !!!       create a first user
+        /*// Create a new user with a first and last name
         val user = hashMapOf(
             "first" to "Ada",
             "last" to "Lovelace",
             "born" to 1815
         )
 
-// Add a new document with a generated ID
+        // Add a new document with a generated ID
         db.collection("users")
             .add(user)
             .addOnSuccessListener { documentReference ->
@@ -29,10 +31,43 @@ class MainActivity : AppCompatActivity() {
             }
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
+            }*/
+
+
+// !!!       create a second user
+        /*// Create a new user with a first, middle, and last name
+        val user = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
             }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }*/
 
 
+// !!!       Read data from database
+        /*db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }*/
 
+
+// !!!       create multiple users at ones
         /*val usersCollection = db.collection("users")
 
         // Create a new user with a first and last name
